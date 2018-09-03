68e45824b20c18f3cc7f094bbedc3e0b728a64e7
schon094 <35474566+schon094@users.noreply.github.com>
2018-09-02 20:11:58 -0500

    Update extract_and_compile.sh
    this still fails test extract_and_compile.sh compiles the source
    executable NthPrime is in the wrong directory

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -8,6 +8,10 @@ name=$2
 
 tar -xzf NthPrime.tgz -C $name
 
-gcc -o $name/NthPrime/NthPrime  $name/NthPrime/main.c $name/NthPrime/nth_prime.c $name/NthPrime/nth_prime.h
+gcc -o NthPrime  $name/NthPrime/main.c $name/NthPrime/nth_prime.c $name/NthPrime/nth_prime.h
+
+#gcc -o $name/NthPrime/NthPrime  $name/NthPrime/main.c $name/NthPrime/nth_prime.c $name/NthPrime/nth_prime.h
 
 ./NthPrime $number
+
+#./$name/NthPrime/NthPrime $number
