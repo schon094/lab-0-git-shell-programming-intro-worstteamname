1da26a1350d218376db9fc79fb76f326da8d681f
munnsmunns <mmunns16@gmail.com>
2018-09-05 21:08:28 -0500

    add comments

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -6,12 +6,12 @@ name=$2
 #echo $number 
 #echo $name
 
+#extract directory
 tar -xzf NthPrime.tgz -C $name
 
+#compile Nth Prime
 gcc -o NthPrime  $name/NthPrime/main.c $name/NthPrime/nth_prime.c $name/NthPrime/nth_prime.h
 
-#gcc -o $name/NthPrime/NthPrime  $name/NthPrime/main.c $name/NthPrime/nth_prime.c $name/NthPrime/nth_prime.h
-
+#execute Nth Prime
 ./NthPrime $number
 
-#./$name/NthPrime/NthPrime $number
